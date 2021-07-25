763819676cb084548a14b3a95bb8232ee7c8c139
ArduFox <47301786+ArduFox@users.noreply.github.com>
2021-07-25 14:07:38 +0200

    Update deb_speed.py
    looping with sleep

--- cron_trouble/deb_speed.py
+++ cron_trouble/deb_speed.py
@@ -1,4 +1,6 @@
 import speedtest
+import datetime
+import time
 
 servers = []
 # If you want to test against a specific server
@@ -10,41 +12,58 @@ threads = None
 
 print("---------------------------------------------")
 print("deb_speed debugging speedtest inside cron job")
+print(datetime.datetime.now().strftime('%d.%m.%y %H:%M KW %V'))
 print("---------------------------------------------\n")
 
 s = speedtest.Speedtest()
-s.get_servers(servers)
+condition = True
 
-print("\nServer dict with " +str(len(s.servers)) + " entries" )
-if (len(s.servers)> 0):
-    k = list(s.servers.keys())
-    #print(k[0])
-    print("first entry:\n" +str(s.servers[k[0]]))
-    print("\nlast entry:\n"+ str(s.servers[k[-1]]))
+print("---------------------------------------------")
+print("deb_speed debugging speedtest inside cron job")
+print(datetime.datetime.now().strftime('%d.%m.%y %H:%M KW %V'))
+print("---------------------------------------------\n")
 
-    s.get_closest_servers()
 
-    print("\n\nclosest Servers list with " + str(len(s.closest)) + " entries" )
-    if (len(s.closest)> 0) :
-        print("first entry:\n"+ str(s.closest[0]))
-        print("\n last entry:\n"+ str(s.closest[-1]))
+loops=0
+while condition:
+    loops= loops+1
+    s.get_servers(servers)
 
-        s.get_best_server()
+    condition = (loops < 5) and (len(s.servers) == 0)
 
-        print("\n\nbest Servers dict with " + str(len(s.best)) + " attributes" )
-        if (len(s.best)> 0):
-            print(s.best)
-            
+    print(str(loops)+": server dict with " +str(len(s.servers)) + " entries" )
 
-            s.download(threads=threads)
-            s.upload(threads=threads)
-            s.results.share()
+    if (len(s.servers)== 0):
+        time.sleep(6)      # sleep for 6 seconds
+    else:
+        k = list(s.servers.keys())
+        #print("first entry:\n" +str(s.servers[k[0]]))
+        #print("\nlast entry:\n{s.servers[k[-1]]})
 
-            print("\n-----------------")
-            print(  "results from test")
-            print(  "-----------------")
+        s.get_closest_servers()
 
-            print (s.results.dict())
+        print("\nclosest Servers list with " + str(len(s.closest)) + " entries" )
+        print("-------------------------------------")
+        if (len(s.closest)> 0) :
+            print("first entry:\n"+ str(s.closest[0]))
+            print("\nlast entry:\n"+ str(s.closest[-1]))
+
+            s.get_best_server()
+
+            print("\n\nbest Servers dict with " + str(len(s.best)) + " attributes" )
+            print("-----------------------------------------")
+            if (len(s.best)> 0):
+                print(s.best)
+
+                s.download(threads=threads)
+                s.upload(threads=threads)
+                s.results.share()
+
+                print("\n-----------------")
+                print(  "results from test")
+                print(  "-----------------")
+
+                print (s.results.dict())
 
 print("done.\n")
 

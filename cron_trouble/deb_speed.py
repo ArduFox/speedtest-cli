7938828bae204160f63ebcadea62fe5c38d257d8
ArduFox <47301786+ArduFox@users.noreply.github.com>
2021-07-25 13:12:10 +0200

    Create deb_speed.py
    first test tool

---
+++ cron_trouble/deb_speed.py
@@ -0,0 +1,39 @@
+import speedtest
+
+servers = []
+# If you want to test against a specific server
+# servers = [1234]
+
+threads = None
+# If you want to use a single threaded test
+# threads = 1
+
+s = speedtest.Speedtest()
+s.get_servers(servers)
+
+print("\nServer dict with " +str(len(s.servers)) + " entries" )
+if (len(s.servers)> 0):
+    k = list(s.servers.keys())
+    #print(k[0])
+    print("first entry:\n" +str(s.servers[k[0]]))
+    print("\nlast entry:\n"+ str(s.servers[k[-1]]))
+
+    s.get_closest_servers()
+
+    print("\n\nclosest Servers list with " + str(len(s.closest)) + " entries" )
+    if (len(s.closest)> 0) :
+        print("first entry:\n"+ str(s.closest[0]))
+        print("\n last entry:\n"+ str(s.closest[-1]))
+
+        s.get_best_server()
+
+        print("\n\nbest Servers dict with " + str(len(s.best)) + " attributes" )
+        if (len(s.best)> 0):
+            print(s.best)
+
+
+
+
+
+
+
